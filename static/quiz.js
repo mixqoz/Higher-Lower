c03429f2e60734badd53061111c901e67528135c
mixqoz <unknownmixqo@gmail.com>
2025-12-09 11:14:24 +0100

    Quiz setup & questions

---
+++ static/quiz.js
@@ -0,0 +1,82 @@
+const questions = [
+	{
+		question: "What's the maximum round time in standard competetive CS:GO2?",
+		optionA: "2 minutes",
+		optionB: "1 minute and 55 seconds", // Correct
+		optionC: "1 minute and 45 seconds",
+		optionD: "1 minute and 30 seconds",
+		correctOption: "optionB",
+	},
+	{
+		question: "What is the name of the bombsites on Mirage?",
+		optionA: "Mid & Top-Mid",
+		optionB: "A & C",
+		optionC: "A & B", // Correct
+		optionD: "B & C",
+		correctOption: "optionC",
+	},
+	{
+		question: "How many players are there on each team?",
+		optionA: "8",
+		optionB: "7",
+		optionC: "6",
+		optionD: "5", // Correct
+		correctOption: "optionD",
+	},
+	{
+		question: "How long does it take to defuse a bomb with a kit?",
+		optionA: "10 seconds",
+		optionB: "8 seconds",
+		optionC: "5 seconds", // Correct
+		optionD: "3 seconds",
+		correctOption: "optionC",
+	},
+	{
+		question: "Which rifle has the highest armor penetration?",
+		optionA: "FAMAS",
+		optionB: "AK-47",
+		optionC: "AWP", // Correct
+		optionD: "M4A4",
+		correctOption: "optionC",
+	},
+	{
+		question: "Which grenade can extinguish a molotov?",
+		optionA: "Smoke", // Correct
+		optionB: "HE Grenade",
+		optionC: "Flashbang",
+		optionD: "Decoy",
+		correctOption: "optionA",
+	},
+	{
+		question: "How much does a Zeus cost?",
+		optionA: "500",
+		optionB: "400",
+		optionC: "300",
+		optionD: "200", // Correct
+		correctOption: "optionD",
+	},
+	{
+		question: "Which map has the fastest rotation time between both bombsites?",
+		optionA: "Ancient",
+		optionB: "Dust II",
+		optionC: "Nuke", // Correct
+		optionD: "Anubis",
+		correctOption: "optionC",
+	},
+	{
+		question: "What is the cost of firing a single M249 bullet?",
+		optionA: "17", // Correct
+		optionB: "15",
+		optionC: "12",
+		optionD: "10",
+		correctOption: "optionA",
+	},
+	{
+		question: "What is the maximum round loss bonus?",
+		optionA: "$3500",
+		optionB: "$3400", // Correct
+		optionC: "$3300",
+		optionD: "$3200",
+		correctOption: "optionB",
+	}
+]
